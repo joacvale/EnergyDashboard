c2ae8cbc6c3825cc846ac5cc7b02e6cd126c3629
Joao Valentim <joacvale@emeal.nttdata.com>
2026-07-14 13:58:28 +0100

    Dataset do chart dividido por tipo de production

--- solar-dashboard/src/app/pages/production-component/production-component.ts
+++ solar-dashboard/src/app/pages/production-component/production-component.ts
@@ -112,24 +112,28 @@ export class ProductionComponent implements AfterViewInit {
 
       datasets: [
         {
-          label: 'ver dados',
-
-          data: data.map(
-            item => item.production
+          label: 'Production',
+          data: data.map(item => 
+            item.type==='production'
+            ? item.production : null
           ),
-
-          backgroundColor: data.map(item => {
-            switch (item.type) {
-              case 'production':
-                return 'rgba(39, 174, 96, 0.8)';
-              case 'limited':
-                return 'rgba(155, 89, 182, 0.8)';
-              case 'idle':
-                return 'rgba(52, 152, 219, 0.8)';
-              default:
-                return 'rgba(0, 0, 0, 0.08)';
-            }
-          }),
+          backgroundColor: 'rgba(39, 174, 96, 0.8)',
+        },
+        {
+          label: 'Limited',
+          data: data.map(item => 
+            item.type==='limited'
+            ? item.production : null
+          ),
+          backgroundColor: 'rgba(155, 89, 182, 0.8)',
+        },
+        {
+          label: 'Idle',
+          data: data.map(item => 
+            item.type==='idle'
+            ? item.production : null
+          ),
+          backgroundColor: 'rgba(52, 152, 219, 0.8)',
         }
       ]
     };
@@ -162,6 +166,7 @@ export class ProductionComponent implements AfterViewInit {
     return `H${peak.hour}`;
 
   });
+  
 
   barChartOptions: ChartOptions<'bar'> = {
     responsive: true,
