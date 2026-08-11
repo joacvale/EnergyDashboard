128a76fdeb0926386ecdd272537f270dcc18406f
Joao Valentim <joacvale@emeal.nttdata.com>
2026-08-11 12:42:30 +0100

    feat(#7-tooltip)

--- solar-dashboard/src/app/pages/merit-order-component/merit-order-component.ts
+++ solar-dashboard/src/app/pages/merit-order-component/merit-order-component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, computed } from '@angular/core';
 import { MeritOrderStore, Block } from '../../stores/merit-order.store';
 import { CdkDropList, CdkDrag, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-merit-order-component',
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, MatTooltip],
   standalone: true,
   templateUrl: './merit-order-component.html',
   styleUrl: './merit-order-component.scss',
@@ -34,6 +35,14 @@ export class MeritOrderComponent {
   drop(event: CdkDragDrop<Block[]>, period: number) {
     this.meritOrderStore.changeBlocksPositions(period, event.previousIndex, event.currentIndex);
   }
+
+  getTooltip(block: Block): string {
+    return `${block.label}
+
+Up program aFRR - ${block.programValue} MW
+Up qFFR price - ${block.offerPrice} €/MWh
+Up aFRR - ${block.bandPercentage}%`;
+  } 
 
   constructor() {
     this.meritOrderStore.loadMeritOrder();
